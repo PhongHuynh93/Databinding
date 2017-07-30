e3f29d9adc4e10f6719363b82031b08e8a93f48f
Phong Huỳnh <p7157568@gmail.com>
2017-07-30 22:35:46 +0700

    feat - create model via xml, java. create constructor for model. create observablefield for model

--- app/src/main/java/example/test/phong/databinding/MainActivity.java
+++ app/src/main/java/example/test/phong/databinding/MainActivity.java
@@ -1,52 +1,27 @@
 package example.test.phong.databinding;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
+import example.test.phong.databinding.databinding.ActivityMainBinding;
+
+
+/**
+ * https://android.jlelse.eu/data-binding-a-library-to-remember-1a5ab7bfc21d
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        setContentView(R.layout.activity_main);
+        // info - How to avoid findViewById():
+//        Inside your onCreate method initialize the data binding object by using DataBindingUtil than use that object to access all your views directly.
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        // info - how to set var via java fiile
+        RegistrationModel registrationModel = new RegistrationModel("Phong", "quoc lo 1a", 20, true);
+        activityMainBinding.setRegistration(registrationModel);
     }
 }
---
+++ app/src/main/java/example/test/phong/databinding/RegistrationModel.java
@@ -0,0 +1,31 @@
+package example.test.phong.databinding;
+
+import android.databinding.ObservableBoolean;
+import android.databinding.ObservableField;
+import android.databinding.ObservableInt;
+
+/**
+ * Created by user on 7/30/2017.
+ * It sets the field with observable which continuously monitors for any changes in the contents.
+ *
+ * No getters and setters?
+ *
+ * You may use like name.set(“Jack”) and name.get() to retrieve value.
+ */
+public class RegistrationModel {
+    public ObservableField<String> name = new ObservableField<>();
+    public ObservableField<String> address = new ObservableField<>();
+    public ObservableInt age = new ObservableInt();
+    public ObservableBoolean isMinor = new ObservableBoolean();
+
+    // info - Using Constructor:
+    public RegistrationModel(String name, String address, int age, boolean isMinor) {
+        this.name.set(name);
+        this.address.set(address);
+        this.age.set(age);
+        this.isMinor.set(isMinor);
+    }
+
+    public RegistrationModel() {
+    }
+}
